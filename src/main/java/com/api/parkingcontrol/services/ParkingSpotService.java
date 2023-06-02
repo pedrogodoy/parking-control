9d1da2f7928036580a9b03e4d37e6277bd158e47
Pedro <pedrogh.godoy12@gmail.com>
2023-06-02 10:42:40 -0300

    first commit

---
+++ src/main/java/com/api/parkingcontrol/services/ParkingSpotService.java
@@ -0,0 +1,33 @@
+package com.api.parkingcontrol.services;
+
+import com.api.parkingcontrol.models.ParkingSpotModel;
+import com.api.parkingcontrol.repositories.ParkingSpotRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ParkingSpotService {
+    final ParkingSpotRepository parkingSpotRepository;
+
+    public ParkingSpotService(ParkingSpotRepository parkingSpotRepository) {
+        this.parkingSpotRepository = parkingSpotRepository;
+    }
+
+    @Transactional
+    public ParkingSpotModel save(ParkingSpotModel parkingSpotModel) {
+        return parkingSpotRepository.save(parkingSpotModel);
+    }
+
+    public boolean existsByLicensePlateCar(String licensePlateCar) {
+        return parkingSpotRepository.existsByLicensePlateCar((licensePlateCar));
+    }
+
+    public boolean existsByParkingSpotNumber(String parkingSpotNumber) {
+        return parkingSpotRepository.existsByParkingSpotNumber(parkingSpotNumber);
+    }
+
+    public boolean existsByApartmentAndBlock(String apartment, String block) {
+        return parkingSpotRepository.existsByApartmentAndBlock(apartment, block);
+    }
+}
